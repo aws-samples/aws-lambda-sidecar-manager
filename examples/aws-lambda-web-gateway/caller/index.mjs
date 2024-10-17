dc66561efe6892d4b6cb834397f215f5bedddf30
DiscreteTom <discrete_tom@outlook.com>
2024-10-17 06:07:58 +0000

    examples: update aws-lambda-web-gateway

--- examples/aws-lambda-web-gateway/caller/index.mjs
+++ examples/aws-lambda-web-gateway/caller/index.mjs
@@ -1,5 +1,7 @@
 export const handler = async () => {
+  const now = Date.now();
   const res = await fetch("http://localhost:8000");
+  console.log(`fetch: ${Date.now() - now}ms`);
 
   return {
     statusCode: 200,
